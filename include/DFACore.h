41b3a4d8215390080cdf23775f9f8704191f5b15
sxf <sun_xfancy@163.com>
2015-11-10 09:54:13 +0800

    修复了诸多问题，成功支持Unicode 尚有一个MainDFA构造bug需要解决
    Signed-off-by: sxf <sun_xfancy@163.com>

--- include/DFACore.h
+++ include/DFACore.h
@@ -3,25 +3,31 @@
 #include "DFA.h"
 #include "Lex.h"
 #include "EquivalenceClass.h"
+
 class DFACore
 {
 public:
     DFACore() {}
-    ~DFACore() {}
-    DFACore(DFA* _dfa,const wchar_t* _d,EquivalenceClass* _pEClass) { Init(_dfa,_d,_pEClass); }
-    void Init(DFA* _dfa,const wchar_t* _d,EquivalenceClass* _pEClass) {
-        dfa = _dfa; data = _d; pEClass = _pEClass;
-        point = 0; row_point =0; line_point = 0;
+    ~DFACore() { if (t != NULL) delete t; }
+    
+    DFACore(DFA* _dfa, const char* _d, EquivalenceClass* _pEClass) { 
+        Init(_dfa,_d,_pEClass); 
     }
-    LexToken* Read();
+
+    void Init(DFA* _dfa, const char* _d, EquivalenceClass* _pEClass) {
+        dfa = _dfa; data = _d; pEClass = _pEClass;
+        point = 0; row_point =1; line_point = 0;
+    }
+
+    Token* Read();
 
 private:
     DFA* dfa;
     int state;
-    const wchar_t* data;
-    int point, row_point, line_point;
-
-    wstring tokendata;
+    Glib::ustring tokendata; // 当前的token数据
+    Glib::ustring data; // 全文件数据
+    int point, row_point, line_point; //处理位置指针
+    Token* t = NULL; // 上一次的token
     EquivalenceClass* pEClass;
 };
 
